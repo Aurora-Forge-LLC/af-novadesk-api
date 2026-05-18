32b960d37938921f7348c8ddbab7a28326cf92dd
Aashutosh Adhikari <coolaashu55@gmail.com>
2026-05-18 23:25:31 +0545

    update test file

--- src/test/java/com/af/novadesk/api/finance/controller/FinanceApiIntegrationTest.java
+++ src/test/java/com/af/novadesk/api/finance/controller/FinanceApiIntegrationTest.java
@@ -42,10 +42,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @Transactional
 @ActiveProfiles("local")
 @TestPropertySource(properties = {
-        "spring.flyway.enabled=false",
-        "spring.datasource.url=jdbc:h2:mem:testdb;MODE=PostgreSQL;INIT=CREATE SCHEMA IF NOT EXISTS af_novadesk\\;CREATE SCHEMA IF NOT EXISTS af_novadesk_outbox",
-        "spring.jpa.properties.hibernate.default_schema=af_novadesk",
-        "spring.jpa.properties.hibernate.hbm2ddl.create_namespaces=true"
+        "spring.flyway.enabled=false"
 })
 class FinanceApiIntegrationTest {
 
