9054719c4c25f921da41d99718906fc9454c9fa3
Roshan <roshanc34@gmail.com>
2026-06-01 11:29:22 +0545

    feat: Inter-Entity Transfer Fix, Exception Handling & Entity Balance Endpoint

--- src/main/java/com/af/novadesk/api/finance/controller/LedgerReportController.java
+++ src/main/java/com/af/novadesk/api/finance/controller/LedgerReportController.java
@@ -4,8 +4,10 @@ import com.af.novadesk.api.common.constants.ApiMessages;
 import com.af.novadesk.api.common.response.ApiResponse;
 import com.af.novadesk.api.common.util.ResponseBuilder;
 import com.af.novadesk.api.finance.api.LedgerReportApi;
+import com.af.novadesk.api.finance.dto.EntityBalanceResponse;
 import com.af.novadesk.api.finance.dto.LedgerReportResponse;
 import com.af.novadesk.api.finance.dto.MultiEntityConsolidatedReport;
+import com.af.novadesk.api.finance.service.EntityBalanceService;
 import com.af.novadesk.api.finance.service.LedgerReportService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
@@ -20,10 +22,15 @@ import java.util.UUID;
 @RestController
 public class LedgerReportController implements LedgerReportApi {
 
-    private final LedgerReportService ledgerReportService;
+    private final LedgerReportService   ledgerReportService;
+    private final EntityBalanceService   entityBalanceService;
 
-    public LedgerReportController(LedgerReportService ledgerReportService) {
-        this.ledgerReportService = ledgerReportService;
+    public LedgerReportController(
+            LedgerReportService ledgerReportService,
+            EntityBalanceService entityBalanceService
+    ) {
+        this.ledgerReportService   = ledgerReportService;
+        this.entityBalanceService  = entityBalanceService;
     }
 
     @Override
@@ -50,5 +57,11 @@ public class LedgerReportController implements LedgerReportApi {
         MultiEntityConsolidatedReport report = ledgerReportService.generateConsolidatedReport(
                 entityIds, startDate, endDate);
         return ResponseBuilder.ok(report, ApiMessages.RECORDS_RETRIEVED_SUCCESS);
+    }
+
+    @Override
+    public ResponseEntity<ApiResponse<EntityBalanceResponse>> getEntityBalance(String entityCode) {
+        EntityBalanceResponse balance = entityBalanceService.getEntityBalance(entityCode);
+        return ResponseBuilder.ok(balance, ApiMessages.RECORDS_RETRIEVED_SUCCESS);
     }
 }
