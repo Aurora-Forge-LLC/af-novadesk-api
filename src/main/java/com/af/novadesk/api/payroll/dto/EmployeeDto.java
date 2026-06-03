255086ee0b6a1c7c6e8a099f4b98c10130c163a5
Roshan <roshanc34@gmail.com>
2026-06-03 17:25:35 +0545

    feat : employee creation two way flow implementation

--- src/main/java/com/af/novadesk/api/payroll/dto/EmployeeDto.java
+++ src/main/java/com/af/novadesk/api/payroll/dto/EmployeeDto.java
@@ -1,6 +1,7 @@
 package com.af.novadesk.api.payroll.dto;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
@@ -71,6 +72,7 @@ public class EmployeeDto {
     private BigDecimal baseSalary;
 
     @NotBlank(message = "Salary currency is required")
+    @JsonProperty("currencyCode")
     private String salaryCurrency;
 
     // Bank details
